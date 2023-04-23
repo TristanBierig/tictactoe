c39b51bca294371bf407b1c4b5f6263a0c673f99
Tristan Bierig <trista.bierig@gmail.com>
2023-04-23 13:09:04 +0200

    bugfixes

--- script.js
+++ script.js
@@ -4,20 +4,22 @@ let currentShape = 'cross';
 
 
 function fillShape(id) {
-    if (currentShape == 'cross') {
-        currentShape = 'circle';
-        document.getElementById('player-2').classList.remove('player-inactive');
-        document.getElementById('player-1').classList.add('player-inactive');
-    } else {
-        currentShape = 'cross';
-        document.getElementById('player-2').classList.add('player-inactive');
-        document.getElementById('player-1').classList.remove('player-inactive');
-    }
+    if (!fields[id]) {
+        if (currentShape == 'cross') {
+            currentShape = 'circle';
+            document.getElementById('player-2').classList.remove('player-inactive');
+            document.getElementById('player-1').classList.add('player-inactive');
+        } else {
+            currentShape = 'cross';
+            document.getElementById('player-2').classList.add('player-inactive');
+            document.getElementById('player-1').classList.remove('player-inactive');
+        }
 
-    fields[id] = currentShape;
-    console.log(fields);
-    draw();
-    checkForWin();
+        fields[id] = currentShape;
+        console.log(fields);
+        draw();
+        checkForWin();
+    }
 }
 
 
